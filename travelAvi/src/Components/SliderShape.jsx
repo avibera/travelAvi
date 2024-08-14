bf70d6734a302351cecccab2b79426acc6450be9
avinash <avinash.b@mindflur.com>
2024-08-15 00:14:38 +0530

    home page length decrease

--- travelAvi/src/Components/SliderShape.jsx
+++ travelAvi/src/Components/SliderShape.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import TextInput from './TextInput'
+import React from "react";
+import TextInput from "./TextInput";
 
-export const SliderShape = ({children}) => {
+export const SliderShape = ({ children }) => {
   return (
-      <div className="absolute w-full z-10">
-          <div>
-              <img
-                  src="https://demo.bosathemes.com/html/travele/assets/images/slider-pattern.png"
-                  className="w-full mt-[-60px] bg-no-repeat bg-center object-cover"
-              />
-          </div>
-          <div className="flex justify-center mt-[-150px]">
-              {/* <div className="grid grid-cols-4 gap-6 bg-white p-12 shadow-xl">
+    <div className="absolute w-full z-10">
+      <div>
+        <img
+          src="https://demo.bosathemes.com/html/travele/assets/images/slider-pattern.png"
+          className="w-full mt-[-60px] bg-no-repeat bg-center object-cover"
+        />
+      </div>
+      <div className="flex justify-center mt-[-150px]">
+        {/* <div className="grid grid-cols-4 gap-6 bg-white p-12 shadow-xl">
                   <TextInput labelName="Package" placeholder="Select package" />
                   <TextInput labelName="Phone" placeholder="Enter your phone" />
                   <TextInput labelName="Phone" placeholder="Enter your phone" />
                   <TextInput labelName="Phone" placeholder="Enter your phone" /> 
               </div> */}
-              {children}
-          </div>
+        {children}
       </div>
+    </div>
   );
-}
+};
--- travelAvi/src/Components/SwipeSlider.jsx
+++ travelAvi/src/Components/SwipeSlider.jsx
@@ -41,7 +41,7 @@ export const SwipeSlider = () => {
               <div className="w-full max-auto">
                 <img
                   src={item?.path}
-                  className="w-full h-[800px] object-cover"
+                  className="w-full h-[740px] object-cover"
                 />
                 <div className="absolute inset-0 bg-gray-800 opacity-30"></div>
                 <div className="absolute inset-0 flex justify-center items-center mt-[-100px]">
